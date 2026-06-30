9d7346a0b8ac07efd1f2d31867350de92389d2af
Gatlink <tyganit@gmail.com>
2026-06-30 20:15:57 +0200

    Stuff

--- Tuto/game.gd
+++ Tuto/game.gd
@@ -11,12 +11,18 @@ const EMOJIS := "☠️,🤖,🐕,🐈,🐅,🐎,🦏,🐄,🐖,🐑,🐪,🦘,�
 	+ "🐿️,🪱,🌕,☀️,💧,🔥,☂️"
 
 
+@export var to_find: Array[EmojiData]
+
+
 var deck: Array[String] = []
 var grid: Array[String]
+var targets: Array[EmojiData] = []
+var current_target: EmojiData
 
 
 func _ready() -> void:
 	fill_grid()
+	current_target = get_next_target()
 
 
 func fill_grid() -> void:
@@ -36,3 +42,18 @@ func shuffle_deck() -> void:
 	for emoji in EMOJIS.split(","):
 		deck.append(emoji)
 	deck.shuffle()
+
+
+func shuffle_targets(combo_count: int) -> void:
+	targets.clear()
+	for target in to_find:
+		var count := target.combo.count(",")
+		if count == combo_count:
+			targets.append(target)
+	targets.shuffle()
+
+
+func get_next_target() -> EmojiData:
+	if targets.is_empty():
+		shuffle_targets(2)
+	return targets.pop_front()
